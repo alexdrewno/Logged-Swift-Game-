7daa773e76640669ca3a183b632066aa05a323ad
Student <student@adrewno1.local>
2015-11-13 11:41:55 -0600

    Initial Commit

---
+++ LoggedTests/LoggedTests.swift
@@ -0,0 +1,36 @@
+//
+//  LoggedTests.swift
+//  LoggedTests
+//
+//  Created by adrewno1 on 11/13/15.
+//  Copyright © 2015 adrewno1. All rights reserved.
+//
+
+import XCTest
+@testable import Logged
+
+class LoggedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
